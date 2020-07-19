871a32cbb54c9a99bfe5303b48a7893bee195ff4
KodaiUchiyama <32435168+KodaiUchiyama@users.noreply.github.com>
2020-07-20 00:30:29 +0900

    delete dist

--- dist/js/chunk-2d210c47.24b86561.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d210c47"],{b8fa:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Contact me")])])}],u=t("2877"),a={},l=Object(u["a"])(a,c,r,!1,null,null,null);n["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d210c47.24b86561.js.map
