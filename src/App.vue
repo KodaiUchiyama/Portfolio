e9e3bd51e1597e6c241033c7ce0700edef8a7f9a
KodaiUchiyama <32435168+KodaiUchiyama@users.noreply.github.com>
2020-07-20 00:07:20 +0900

    logoにリンク付加

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
   <v-app>
     <!--<v-app-bar flat color="primary lighten-5" app>-->
     <v-app-bar flat elevate-on-scroll color="shades white" app class="pt-10" style="padding-bottom:90px;">
+      <router-link to="/">
       <v-img
         class="mx-2"
         src="./assets/logo.png"
         max-height="70"
         max-width="70"
         contain
-        style="margin: 0 auto;">
+        style="cursor:pointer;margin: 0 auto;">
       </v-img>
+      </router-link>
       <v-toolbar-title style="cursor:pointer;" @click="$router.push('/').catch(()=>{})" class="mx-2 font-weight-medium hidden-xs-only">Kodai Uchiyama</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-        <v-btn class="text-none" text tile to="/" id="">Work</v-btn>
-        <v-btn class="text-none" text tile to="/about" id="">About</v-btn>
+        <v-btn class="text-none" text tile to="/">Work</v-btn>
+        <v-btn class="text-none" text tile to="/about">About</v-btn>
     </v-app-bar>
 
     <v-main class="my-7">
